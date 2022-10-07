0ff762bb2eaee9445848920a59ba4dedd4d0e551
choi-hyeseong <bd2845@kakao.com>
2022-10-07 17:31:03 +0900

    initial Commit

---
+++ src/main/java/com/division/springbootstudy/SpringBootStudyApplication.java
@@ -0,0 +1,13 @@
+package com.division.springbootstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudyApplication {
+    //Spring Main
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStudyApplication.class, args);
+    }
+
+}
